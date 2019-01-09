6d76d55c47ea0a180b311c91a5c41967f69c8feb
Tom Eloe <tomeloe@gmail.com>
2019-01-08 17:31:15 -0800

    Organize files

---
+++ Data Structures/Arrays/Arrays - DS/solution.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// https://www.hackerrank.com/challenges/arrays-ds/problem
+
+function reverseArray(a) {
+  return a.reverse();  
+}
+
+console.log(reverseArray([1, 4, 3, 2])); // [2, 3, 4, 1] 
