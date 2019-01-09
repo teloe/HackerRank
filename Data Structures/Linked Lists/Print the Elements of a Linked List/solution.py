12d03dc123120b3767d26924993987c875410cfa
Tom Eloe <tomeloe@gmail.com>
2019-01-09 08:57:36 -0800

    udpate solution.py

--- Data Structures/Linked Lists/Print the Elements of a Linked List/solution.py
+++ Data Structures/Linked Lists/Print the Elements of a Linked List/solution.py
@@ -1,6 +1,6 @@
 # https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem
 
-def printLinkedList(head):
+def print_list(head):
     if head is not None:
         print(head.data)
         print_list(head.next)
