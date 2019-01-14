38fcf795a894a70738ef06fdc00a5e7c91e94a69
Tom Eloe <tomeloe@gmail.com>
2019-01-14 09:33:23 -0800

    update circular-array.js/ README.md

--- README.md
+++ README.md
@@ -10,7 +10,7 @@
 </p>
 
 <p align="center">
-  <img src="https://img.shields.io/badge/Problems%20Solved-57-brightgreen.svg" alt="problems solved">
+  <img src="https://img.shields.io/badge/Problems%20Solved-62-brightgreen.svg" alt="problems solved">
   <img src="https://img.shields.io/badge/Language-JavaScript-blue.svg" alt="problems solved">
 </p>
 
--- algorithms/implementation/circular-array-rotation.js
+++ algorithms/implementation/circular-array-rotation.js
@@ -9,6 +9,14 @@
  */
 
 function circularArrayRotation(a, k, queries) {
+    if (k > a.length) k = k % a.length;
+    var aa = a.splice(a.length - k, k + 1);
+    a.unshift(...aa);
+
+    return queries.map((x) => {
+        return a[x];
+    });
+
 }
 
-console.log(circularArrayRotation(a, k, queries)); //
+console.log(circularArrayRotation([1, 2, 3], 2, [0, 1, 2])); // [2, 3, 1]
